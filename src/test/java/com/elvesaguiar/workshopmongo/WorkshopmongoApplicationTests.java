1da487323f0e760895e501ed8352ec96acf7c72d
Elves Aguiar <elvesmhs@hotmail.com>
2022-11-30 08:55:00 -0300

    initial commit

---
+++ src/test/java/com/elvesaguiar/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvesaguiar.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
