7687ee8538e8bc7de80f3e04ab7ecd4c6afb0cf8
Elves Aguiar <elvesmhs@hotmail.com>
2022-12-01 11:26:59 -0300

    feat: insert and update

--- src/main/java/com/elvesaguiar/workshopmongo/resources/UserResource.java
+++ src/main/java/com/elvesaguiar/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.elvesaguiar.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.elvesaguiar.workshopmongo.domain.User;
 import com.elvesaguiar.workshopmongo.dto.UserDTO;
@@ -49,6 +54,23 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@RequestMapping(method=RequestMethod.POST)
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
+	@PutMapping (value="/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 
 }
