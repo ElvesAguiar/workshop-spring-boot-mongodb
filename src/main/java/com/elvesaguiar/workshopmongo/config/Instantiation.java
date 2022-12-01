2521ecc81f416caed685aca2a619ec9f8ff4fecd
Elves Aguiar <elvesmhs@hotmail.com>
2022-12-01 18:39:20 -0300

    feat: implementing AuthorDTO

--- src/main/java/com/elvesaguiar/workshopmongo/config/Instantiation.java
+++ src/main/java/com/elvesaguiar/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.elvesaguiar.workshopmongo.domain.Post;
 import com.elvesaguiar.workshopmongo.domain.User;
+import com.elvesaguiar.workshopmongo.dto.AuthorDTO;
 import com.elvesaguiar.workshopmongo.repository.PostRepository;
 import com.elvesaguiar.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Abraços",maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei",maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Abraços",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei",new AuthorDTO(maria));
+		
+		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
